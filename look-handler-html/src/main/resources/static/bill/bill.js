bd74a2c8eecc1318911cb7b694e99ffad1ad0da4
Administrator <Administrator@MS-JEKHONYYXDCZ>
2022-12-12 20:42:58 +0800

    feat: 添加长按下载图片

--- look-handler-html/src/main/resources/static/bill/bill.js
+++ look-handler-html/src/main/resources/static/bill/bill.js
@@ -1,7 +1,17 @@
+var timeCreate = null;
 $(function() {
 	let curHeight = document.body.scrollHeight;
 	$("#main").css("height", curHeight + "px");
 	$("#bill").hide();
+	
+	//手机浏览器长按事件
+	$(".save").on("touchstart", function(e) {createImg();});
+	$(".save").on("touchend", function(e) {cancleImg();});
+	//微信录音
+	getConfig(function(res) {
+		if(res.code == 200) {initWx(res.data);}
+		else {alert(res.msg);}
+	});
 });
 
 //初始微信配置
@@ -14,57 +24,32 @@ function initWx(wxConfig) {
 	    nonceStr: wxConfig.nonceStr, // 必填，生成签名的随机串
 	    signature: wxConfig.signature,// 必填，签名，见附录1
 	    jsApiList: [
-          "onMenuShareAppMessage"
+          "onMenuShareAppMessage",
+          "updateAppMessageShareData",
         ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
 	});
 	wx.checkJsApi({
         jsApiList: [
-          "onMenuShareAppMessage"
+          "onMenuShareAppMessage",
+          "updateAppMessageShareData",
         ],
     	fail: res => {
      	  alert("您的微信版本太低，请使用最新的微信客户端！" + res);
     	}
   	});
 	wx.ready(function(){
-		wx.onMenuShareAppMessage({
+		wx.updateAppMessageShareData({
 		  title: '以“云”为媒 共读二十大', // 分享标题
 		  desc: '奋进新征程，千里共湖湾', // 分享描述
 		  link: 'https://www.huidihao.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号 JS 安全域名一致
 		  imgUrl: 'https://www.huidihao.com/bill/img/shareIcon.png', // 分享图标
-		  type: 'link', // 分享类型,music、video或link，不填默认为link
-		  success: function () {
-		    alert("分享成功！");
+		  success: function (res) {
+		 	console.log("分享成功！", res);
 		  }
-		});
-		wx.onMenuShareTimeline({
-		  title: '以“云”为媒 共读二十大', // 分享标题
-		  link: 'https://www.huidihao.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号 JS 安全域名一致
-		  imgUrl: 'https://www.huidihao.com/bill/img/shareIcon.png', // 分享图标
-		  success: function () {
-		 	alert("分享成功！");
-		  }
-		});
-		wx.onMenuShareQQ({
-		  title: '以“云”为媒 共读二十大', // 分享标题
-		  desc: '奋进新征程，千里共湖湾', // 分享描述
-		  link: 'https://www.huidihao.com', // 分享链接
-		  imgUrl: 'https://www.huidihao.com/bill/img/shareIcon.png', // 分享图标
-		  success: function () {
-		  	alert("分享成功！");
-		  }
-		});
+		})
 	});
 	wx.error(function(res){
 		alert("微信验证失败！" + res);
-	});
-}
-
-//分享数据
-function shareData() {
-	//微信录音
-	getConfig(function(res) {
-		if(res.code == 200) {initWx(res.data);}
-		else {alert(res.msg);}
 	});
 }
 
@@ -87,4 +72,20 @@ function getQrConfig() {
 		height: 60,
 		text: "https://" + location.host
 	};
+}
+
+//创建图片
+function createImg() {
+	timeCreate = setTimeout(function() {
+		html2canvas($("body")[0]).then(function(cvs) {
+			var url = cvs.toDataURL(1);
+			$("#downBill").attr("href", url);
+			$("#downBill")[0].dispatchEvent(new MouseEvent("click"));
+		});
+	}, 1500);
+}
+//取消创建
+function cancleImg() {
+	console.log("结束");
+	clearTimeout(timeCreate);
 }
